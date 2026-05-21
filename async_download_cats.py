dec77895a636b5f23a43c679a32fc0916f4088c7
idotsenko <idotsenko@usergate.com>
2026-05-21 18:28:38 +0300

    awdwadaw

--- async_download_cats.py
+++ async_download_cats.py
@@ -5,7 +5,7 @@ import asyncio
 
 import aiohttp
 import aiofiles
-
+import aiofiles.os
 
 http_proxy = os.getenv('http_proxy', None)
 HTTP_PROXY = os.getenv('HTTP_PROXY', None)
@@ -14,7 +14,9 @@ PROXY = (http_proxy or HTTP_PROXY or None)
 BASE_DIR = Path(__file__).parent
 
 URL = 'https://api.thecatapi.com/v1/images/search'
-CATS_DIR = CATS_DIR = BASE_DIR / 'cats'
+CATS_DIR = BASE_DIR / 'cats'
+
+
 
 
 # Асинхронная функция для получения нового изображения.
@@ -22,7 +24,7 @@ async def get_new_image_url():
     # Создать асинхронную сессию для выполнения HTTP-запроса.
     async with aiohttp.ClientSession() as session:
         # Выполнить асинхронный GET-запрос на указанный URL.
-        response = await session.get(URL)
+        response = await session.get(URL, proxy=PROXY)
         # Асинхронно получить тело ответа в формате JSON.
         data = await response.json()
         # Извлечь URL случайного изображения из ответа.
@@ -33,14 +35,21 @@ async def get_new_image_url():
         return random_cat
 
 
-# Главная асинхронная функция.
+# Определить асинхронную функцию для создания директории.
+async def create_dir(dir_name):
+    # Асинхронно создать директорию.
+    await aiofiles.os.makedirs(
+        dir_name,
+        exist_ok=True
+    )
+
+
 async def main():
-    # Создать список задач для асинхронного выполнения.
+    # Новое — тут создаётся директория с именем cats.
+    await create_dir('cats')
     tasks = [
-        # Асинхронно выполнить функцию get_new_image_url() 100 раз.
         asyncio.ensure_future(download_new_cat_image()) for _ in range(100)
     ]
-    # Подождать, пока выполнятся все задачи.
     await asyncio.wait(tasks)
 
 
@@ -48,7 +57,7 @@ async def main():
 async def download_file(url):
     filename = url.split('/')[-1]
     async with aiohttp.ClientSession() as session:
-        result = await session.get(url)
+        result = await session.get(url, proxy=PROXY)
         # Здесь нужно использовать асинхронный контекстный менеджер.
         async with aiofiles.open(CATS_DIR / filename, 'wb') as f:
             # Перед методом записи нужно добавить ключевое слово await.
@@ -58,6 +67,18 @@ async def download_file(url):
 async def download_new_cat_image():
     url = await get_new_image_url()
     await download_file(url)
+
+
+# hundred_cats/async_download_cats.py
+
+...
+async def list_dir(dir_name):
+    # Асинхронно получить список файлов и поддиректорий в указанной директории.
+    files_and_dirs = await aiofiles.os.listdir(dir_name)
+    # Напечатать каждый элемент содержимого директории, 
+    # разделяя их переносом строки.
+    print(*files_and_dirs, sep='\n')    
+
 
 
 # Точка входа в программу.
@@ -72,5 +93,6 @@ if __name__ == '__main__':
     
     # Записать текущее время окончания выполнения программы.
     end_time = datetime.now()
+    asyncio.run(list_dir(CATS_DIR))
     # Напечатать время выполнения программы.
     print(f'Время выполнения программы: {end_time - start_time}.')
